201df5a85ff3ab076bb246c9892b72f9ccd3eb9c
yanmiao <yanmiao@shinemo.com>
2022-08-21 01:22:01 +0800

    feat : 自动获取本机 ip 地址作为链接

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite';
 import {VantResolver} from 'unplugin-vue-components/resolvers';
+// @ts-ignore
+import getIP from "./src/config/getIP.ts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,7 +25,8 @@ export default defineConfig({
         }),],
     base: './', //打包相对路径
     server: {
-        host: '192.168.31.194',	// 本机的局域网IP
+        // host: '192.168.31.194',	// 本机的局域网IP
+        host: getIP,	// 本机的局域网IP
         port: 2333, //指定端口号
         proxy: {
             // '/user': {

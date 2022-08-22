c7826af1aaaed0300aef2dac166e6111b1334611
yanmiao <yanmiao@shinemo.com>
2022-08-22 10:01:19 +0800

    fix : defineProps 是编译宏, 不需要额外导入

--- src/components/CardBlockItem.vue
+++ src/components/CardBlockItem.vue
@@ -21,9 +21,6 @@
   </div>
 </template>
 <script setup>
-
-import {defineProps} from "vue";
-
 const props = defineProps({
   icon: {
     type: String,
--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script setup>
-import {defineProps, ref} from "vue";
 
 const props = defineProps({
   title: String,

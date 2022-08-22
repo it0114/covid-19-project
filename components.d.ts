d7b226173fc6e02d5e5c2b68f2d03cc1013f8036
yanmiao99 <503084944@qq.com>
2022-08-22 17:36:49 +0800

    feat : 打包文件 - 修复打包报错

--- components.d.ts
+++ components.d.ts
@@ -9,16 +9,11 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CardBlockItem: typeof import('./src/components/CardBlockItem.vue')['default']
     CardBox: typeof import('./src/components/CardBox.vue')['default']
-    VanButton: typeof import('vant/es')['Button']
     VanField: typeof import('vant/es')['Field']
-    VanGrid: typeof import('vant/es')['Grid']
-    VanGridItem: typeof import('vant/es')['GridItem']
-    VanImage: typeof import('vant/es')['Image']
     VanPicker: typeof import('vant/es')['Picker']
     VanPopup: typeof import('vant/es')['Popup']
     VanSticky: typeof import('vant/es')['Sticky']
     VanTab: typeof import('vant/es')['Tab']
     VanTabs: typeof import('vant/es')['Tabs']
-    VanTag: typeof import('vant/es')['Tag']
   }
 }
